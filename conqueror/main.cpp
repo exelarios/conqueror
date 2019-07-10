02b257fa7170d29e92839d3d5e2ab2e1a9f65959
deric <derickwok25@gmail.com>
2019-07-09 19:35:37 -0700

    Initial Commit

---
+++ conqueror/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  conqueror
+//
+//  Created by Deric Kwok on 7/9/19.
+//  Copyright © 2019 Deric Kwok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
